6487b1890c1cfd12459836f22bb1d17a2f23fc41
Axel Ingo <axel.k.ingo@gmail.com>
2023-01-02 16:56:19 +0100

    JS template

--- myjs.js
+++ myjs.js
@@ -1,13 +1,15 @@
 const reqTemplate = {
     method: "GET",
     headers: {
-        Content-Type: "application/json"
+        "Content-Type": "application/json"
     }
 };
 
+let ans
 fetch("https://jsonplaceholder.typicode.com/todos/1",reqTemplate)
     .then(response => response.json())
     .then(data => {
+        ans = data;
         console.log(data);
     })
     .catch(error => {
