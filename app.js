1ee3cc2abf89c8b9843ed6b216d7f351bee1d398
Axel Ingo <axel.k.ingo@gmail.com>
2023-01-03 10:58:57 +0100

    Basic node setup working on localhost

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ http.createServer((req, res) => {
   // get the file path
   let filePath = path.join(
     __dirname,
-    'public',
+    '',
     req.url === '/' ? 'index.html' : req.url
   );
 
@@ -43,7 +43,7 @@ http.createServer((req, res) => {
       if (err.code == 'ENOENT') {
         // page not found
         fs.readFile(
-          path.join(__dirname, 'public', '404.html'),
+          path.join(__dirname, '', '404.html'),
           (err, content) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(content, 'utf8');
